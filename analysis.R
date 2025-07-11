12bcd03586ba1cd5d0282394b182df96a957cc5a
Marwin Carmo <marwinbr@gmail.com>
2025-07-11 09:47:46 -0700

    bggm sim results

--- analysis.R
+++ analysis.R
@@ -1,26 +1,56 @@
 ## Libraries
 
 library(dplyr)
+library(ggplot2)
 
 ## 1.1 Load in results
 
 bda_df <- readRDS("out/df_624.rds")
+
 bggm_df <- rbind(
   readRDS("out/df_bggm_143.rds"),
   readRDS("out/df_bggm_216.rds"))
-rj_df <- readRDS("out/df_rj_528")
+
+rj_df <- readRDS("out/df_rj_648")
+
+sssl_df <-  readRDS("out/results_df_sssl.rds") |>
+  dplyr::select(rep, dplyr::everything())
 
 ## 1.2 Summarise results
 get_summary <- function(df) {
   df |>
   tidyr::pivot_longer(cols = p_plus:relative_strength_mae) |>
-  dplyr::group_by(dplyr::across(c(condition_id, p:name))) |>
+  dplyr::group_by(dplyr::across(c(p:name))) |>
   dplyr::summarise(value = mean(value)) |>
   tidyr::pivot_wider(names_from = name, values_from = value)
 }
 names(bda_df)
 names(bggm_df)
-s_bda <- get_summary(bda_df)
 
+s_bda <- get_summary(bda_df)
+s_sssl <- get_summary(sssl_df)
 s_bggm <- get_summary(bggm_df)
 s_rj <- get_summary(rj_df)
+
+
+## Plots
+### Graph priors
+
+s_rj |>
+  dplyr::filter(b_prior == b_true) |>
+  dplyr::mutate(g_condition =
+                  dplyr::case_when(
+                         g_prior == 0.2 & g_true == 0.2 ~ "p_prior = 0.2, p_true = 0.2",
+                         g_prior == 0.5 & g_true == 0.2 ~ "p_prior = 0.5, p_true = 0.2",
+                         g_prior == 0.8 & g_true == 0.2 ~ "p_prior = 0.8, p_true = 0.2",
+                         g_prior == 0.2 & g_true == 0.5 ~ "p_prior = 0.2, p_true = 0.5",
+                         g_prior == 0.5 & g_true == 0.5 ~ "p_prior = 0.5, p_true = 0.5",
+                         g_prior == 0.8 & g_true == 0.5 ~ "p_prior = 0.8, p_true = 0.8",
+                         g_prior == 0.2 & g_true == 0.8 ~ "p_prior = 0.2, p_true = 0.8",
+                         g_prior == 0.5 & g_true == 0.8 ~ "p_prior = 0.5, p_true = 0.8",
+                         g_prior == 0.8 & g_true == 0.8 ~ "p_prior = 0.8, p_true = 0.8"),
+                p = factor(p), n = factor(n)
+                ) |>
+  ggplot(aes(x = n, y = specificity, color = p)) +
+  geom_boxplot() +
+  facet_grid(g_prior ~ g_true)
